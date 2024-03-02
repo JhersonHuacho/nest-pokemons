82b1f1776c6a2eedf1efb2180cbcbcebabee1396
Francisco <jhersonhuacho@gmail.com>
2024-03-02 15:40:08 -0500

    servir archivo estatico

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { join } from 'path'; // viene en Node
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
+})
+export class AppModule {}
